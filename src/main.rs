6f3e0d4622fcf0fcafa14b9b8c83234cb7bbaa83
Alec Wang <alecwang@Alecs-MBP.home>
2024-07-28 23:57:34 -0700

    ran format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
